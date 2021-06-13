7e572674ee0611eb9328d4e05c20c9c5607acf2e
Eduar Ursu <eduardursu78@gmail.com>
2021-06-13 12:48:08 +0200

    Update projects

--- src/main.js
+++ src/main.js
@@ -1,164 +1,202 @@
 var iso;
-var d3ss = {
-  //Open and close the navbar , on default button ofr navbar or using the toggle parameter
-  navbar: function (toggle) {
-    var n = document.querySelector("nav");
-    if (n) {
-      const toggleActive = function () {
-        n.classList.toggle("opened");
-      };
-      var oc = (toggle) ? toggle : "nav>div>div";
-      document.querySelector(oc).addEventListener("click", toggleActive);
-    }
-  },
-  theme: function () {
-    //Theme Switcher
-    var toggle = document.getElementById("theme-toggle");
-    if (window.CSS && CSS.supports("color", "var(--bg)") && toggle) {
-      var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ?
-        "dark" : "light");
-      if (storedTheme)
-        document.documentElement.setAttribute('data-theme', storedTheme)
-      toggle.onclick = function () {
-        var currentTheme = document.documentElement.getAttribute("data-theme");
-        var targetTheme = "light";
-
-        if (currentTheme === "light") {
-          targetTheme = "dark";
-        }
-
-        document.documentElement.setAttribute('data-theme', targetTheme)
-        localStorage.setItem('theme', targetTheme);
-      };
-    }
-  },
-  aos: function () {
-    //Get and observe all the items with the item class
-    let items = document.querySelectorAll("[class*=_aos]");
-    //Only Use the IntersectionObserver if it is supported and _aos elements exist
-    if (IntersectionObserver && items) {
-      //When the element is visible on the viewport, 
-      //add the _aos-done class so it creates the _aos animation.
-      let callback = function (entries) {
-        entries.forEach(entry => {
-          //if the element is visible, add the _aos-done class
-          if (entry.isIntersecting && !entry.target.classList.contains('_aos-done')) {
-            entry.target.classList.add('_aos-done');
-          } else {
-            //else the element do reverse animation
-            entry.target.classList.remove('_aos-done');
-          }
-        });
-      }
-      //Create the observer
-      let observer = new IntersectionObserver(callback, {
-        root: null,
-        threshold: 0.1
-      });
-      //Add each _aos element to the observer
-      items.forEach((item) => {
-        observer.observe(item);
-      });
-    }
-
-  },
-  gotop: function () {
-    const gt_btn = document.getElementById('gt-link');
-    if (gt_btn) {
-      // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
-      window.addEventListener("scroll", () => {
-        let y = window.scrollY;
-        if (y > 0) {
-          gt_btn.className = "gt-link show";
-        } else {
-          gt_btn.className = "gt-link hide";
-        }
-      });
-      gt_btn.onclick = function (e) {
-        e.preventDefault();
-        if (document.documentElement.scrollTop || document.body.scrollTop > 0) {
-          window.scroll({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-          });
-        }
-      }
-    }
-  },
-  pagesRoute: function () {
-    const notFoundPage = document.querySelector("#notFound");
-    const scrollTop = () => {
-      document.querySelector('html').scrollTop = 0;
-      document.querySelector('body').scrollTop = 0;
-    }
-    const navigate = (id) => {
-      var activePage = document.querySelector("section.page-active");
-      const activeLink = document.querySelector('[topage].active');
-      activeLink.classList.remove("active");
-      const pageTo = document.querySelector('[topage="' + id + '"]');
-      pageTo.classList.add("active");
-      if (activePage) {
-        activePage.classList.remove("page-active");
-      }
-      var nextPage = document.querySelector(id);
-      if (nextPage) {
-        nextPage.classList.add("page-active");
-      } else {
-        if (notFoundPage)
-          notFoundPage.classList.add("page-active");
-      }
-      scrollTop();
-      if (history.pushState) {
-        history.pushState(null, null, id);
-      } else {
-        location.hash = id;
-      }
-
-    }
-    const link = Array.from(document.querySelectorAll('[topage]'))
-    link.forEach(function (page) {
-      var id = page.getAttribute("topage");
-      page.addEventListener('click', function () {
-
-        navigate(id);
-        if(id=="#home"){
-          iso.arrange({
-            filter: '*'
-          });
-        }
-      });
-    })
-    if (window.location.hash) {
-      navigate(window.location.hash);
-    }
+function navbar(){
+  var t = this;
+  t.n = document.querySelector("nav");
+  t.bo = document.body.style.overflow;
+  t.close = function(){
+    t.bo="auto";
+    t.n.classList.remove("active");
+  }
+  t.open = function(){
+    t.bo="hidden";
+    t.n.classList.add("active");
+  }
+  if (t.n) {
+    document.querySelector("nav>button").addEventListener("click", function(){
+      console.log("toggleNav");
+      if(t.n.classList.contains("active"))
+        t.close();
+      else
+        t.open();
+    });
+    document.querySelectorAll("nav ul > a").forEach(n => n.addEventListener("click", t.close()));
   }
 }
+function themeManager(){
+  //Theme Switcher
+  var toggles = document.getElementsByClassName("theme-toggle");
 
+  if (window.CSS && CSS.supports("color", "var(--bg)") && toggles) {
+    var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ?
+      "dark" : "light");
+    if (storedTheme)
+      document.documentElement.setAttribute('data-theme', storedTheme)
+    for(var i=0;i<toggles.length;i++){
+        toggles[i].onclick = function () {
+          var currentTheme = document.documentElement.getAttribute("data-theme");
+          var targetTheme = "light";
+
+          if (currentTheme === "light") {
+            targetTheme = "dark";
+          }
+
+          document.documentElement.setAttribute('data-theme', targetTheme)
+          localStorage.setItem('theme', targetTheme);
+        };
+    }
+    
+  }
+}
+function aos() {
+  //Get and observe all the items with the item class
+  let items = document.querySelectorAll("[class*=_aos]");
+  //Only Use the IntersectionObserver if it is supported and _aos elements exist
+  if (IntersectionObserver && items) {
+    //When the element is visible on the viewport, 
+    //add the _aos-done class so it creates the _aos animation.
+    let callback = function (entries) {
+      entries.forEach(entry => {
+        //if the element is visible, add the _aos-done class
+        if (entry.isIntersecting && !entry.target.classList.contains('_aos-done')) {
+          entry.target.classList.add('_aos-done');
+        } else {
+          //else the element do reverse animation
+          entry.target.classList.remove('_aos-done');
+        }
+      });
+    }
+    //Create the observer
+    let observer = new IntersectionObserver(callback, {
+      root: null,
+      threshold: 0
+    });
+    //Add each _aos element to the observer
+    items.forEach((item) => {
+        observer.observe(item);
+    });
+  }
+}
+function gotop(){
+  var el =this;
+  el.gt =document.getElementById('gt-link');
+  el.scrollToTop= function(){
+    window.scroll({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+  }
+  el.listeners= function(){
+    window.addEventListener("scroll", () => {
+      let y = window.scrollY;
+      if (y > 0) {
+        el.gt.classList.remove("hide");
+      } else {
+        el.gt.classList.add("hide");
+      }
+    });
+    el.gt.onclick = function (e) {
+      e.preventDefault();
+      if (document.documentElement.scrollTop || document.body.scrollTop > 0) {
+        el.scrollToTop();
+      }
+    }
+  }
+  if(el.gt){
+    el.listeners();
+  }
+}
+function pagesRoute(){
+  var t=this;
+  const notFoundPage = document.querySelector("#notFound");
+  t.links = Array.from(document.querySelectorAll('[topage]'));
+  t.scrollTop = ()=>{
+    document.querySelector('html').scrollTop = 0;
+    document.querySelector('body').scrollTop = 0;
+  }
+  t.navigate = (id) =>{
+    //Hide current active page
+    var activePage = document.querySelector("section.page-active");
+    if (activePage) activePage.classList.remove("page-active");
+    var activeLink = document.querySelector('[topage].active');
+    if (activeLink) activeLink.classList.remove("active");
+    //Show the next page
+    var nextPage = document.querySelector(id);
+    if (nextPage) nextPage.classList.add("page-active");
+    var nextLink = document.querySelector("[topage='"+id+"']");
+    if(nextLink) nextLink.classList.add("active");
+    //Scroll to top
+    t.scrollTop();
+    //Set history state
+    if (history.pushState)
+      history.pushState(null, null, id);
+    else 
+      location.hash = id;
+  }
+  t.listeners= ()=>{
+    t.links.forEach((page)=>{
+      var id = page.getAttribute("topage");
+      page.addEventListener('click', ()=>{t.navigate(id)});
+    })
+  }
+  if(t.links){
+    if (window.location.hash)
+        t.navigate(window.location.hash);
+    t.listeners();
+    }
+}
+function filterGrid(){
+  var t =this;
+  t.items = document.querySelectorAll(".filter-container>div");
+  t.flts = Array.from(document.querySelectorAll("[data-filter]"));
+  t.addF=(element,name)=>{
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+  }
+  t.listeners=()=>{
+    t.flts.forEach((element)=>{
+        var c= element.getAttribute("data-filter");
+        element.addEventListener("click",()=>{
+          var current= document.querySelector("[data-filter].active");
+          current.classList.remove("active");
+          element.classList.add("active");
+          t.filter(c)});
+    });
+  }
+  t.removeF=(element,name)=>{
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+  t.filter = (c)=>{
+    console.log(c);
+    if (c == "all") c = "";
+    for (let i = 0; i < t.items.length; i++) {
+      t.removeF(t.items[i], "show");
+      if (t.items[i].className.indexOf(c) > -1) t.addF(t.items[i], "show");
+    }
+  }
+  t.filter("all");
+  t.listeners();
+}
 /* Things done once the DOM is loaded */
 function d3ssOnDOMContentLoaded() {
-  d3ss.navbar();
-  d3ss.theme();
-  d3ss.gotop();
-  d3ss.pagesRoute();
-  d3ss.aos();
-  iso = new Isotope('.flt_cnt', {
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
-  });
-  // filter items on button click
-  var filterButtons = document.querySelectorAll('.flt_btns>button');
-  filterButtons.forEach(element => {
-    element.addEventListener("click", () => {
-      var current = document.querySelector(".flt_btns>button.active");
-      current.classList.remove("active");
-      element.className += " active";
-      var filterValue = element.getAttribute('data-filter');
-      iso.arrange({
-        filter: filterValue
-      });
-    });
-  });
+  navbar();
+  themeManager();
+  gotop();
+  pagesRoute();
+  aos();
+  filterGrid();
   // Play logo animation once
   var d3Logo = document.querySelector('#d3Logo');
   var wstatus = 1;
@@ -173,6 +211,8 @@ function d3ssOnDOMContentLoaded() {
   });
   setTimeout(startA, 300);
 }
+
+
 
 // Call the function when the DOM is loaded
 document.addEventListener("DOMContentLoaded", d3ssOnDOMContentLoaded);
