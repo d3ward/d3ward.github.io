9f7daa4c34eed5136246e8ec0272668b55776181
Eduar Ursu <eduardursu78@gmail.com>
2021-10-02 20:48:18 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -553,9 +553,66 @@ function navbar() {
     }
   }
   customElements.define('footer-component', Footer);
+  
+let text = [ "DEVELOPER + DESIGNER ","Tic Tac Toe","Connect Four","Memory Tiles"];
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+
+
   var n,a,t,s;
 // reload components for each container after transition
 function init() {
+  var elty = document.getElementById("typewriter");
+  if(elty!=undefined){
+        var toRotate = elty.getAttribute('data-type');
+        var period = elty.getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elty, JSON.parse(toRotate), period);
+        }
+  }
+
+  
+
   if(!s) s= new gotop();
   s.scrollToTop();
   if(!n)n =new navbar();
