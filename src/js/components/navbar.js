e1d9f5df2f31314a8e08d0bd09e194abbc183000
Eduard Ursu <eduardursu78@gmail.com>
2022-12-21 10:29:48 +0100

    navbar improvements + grid

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,25 +1,25 @@
 export function navbar() {
 	var t = this
 	t.n = document.querySelector('nav')
-	t.bo = document.body.style.overflow
 	t.close = function () {
-		t.bo = 'auto'
+		document.body.style.overflow = 'auto'
 		t.n.classList.remove('active')
 	}
 	t.open = function () {
-		t.bo = 'hidden'
+		document.body.style.overflow = 'hidden'
 		t.n.classList.add('active')
 	}
 	if (t.n) {
 		document
 			.querySelector('nav>button')
-			.addEventListener('click', function () {
+			.addEventListener('click', ()=>{
 				console.log('toggleNav')
 				if (t.n.classList.contains('active')) t.close()
 				else t.open()
 			})
+		document.querySelector('nav>.nav-overlay').addEventListener('click',()=>{t.close()})
 		document
 			.querySelectorAll('nav ul > a')
-			.forEach((n) => n.addEventListener('click', t.close()))
+			.forEach((n) => n.addEventListener('click', ()=>{t.close()}))
 	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -186,7 +186,7 @@ class Demo {
 			'<label><i gs-icon="watchers"></i><div gs-data="watchers"></div></label>'+
 			'<label><i gs-icon="stars"></i><div gs-data="stars"></div></label>'+
 			'<label><i gs-icon="forks"></i><div gs-data="forks"></div></label></div></div>'
-		div.className = "col-4";
+		div.className = "col-4 _f-center";
 		var url = (data[i].url == null )?'':'<a target="_blank" class="btn btn-p" href="'+data[i].url+'"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path><line x1="10" y1="14" x2="20" y2="4"></line><polyline points="15 4 20 4 20 9"></polyline></svg>Open</a>'
 		var githuburl = (data[i].githuburl == null )?'':'<a target="_blank" class="btn btn-p" href="'+data[i].githuburl+'"><svg id="github-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>Github</a>'
 		div.setAttribute("data-groups",data[i].groups)
