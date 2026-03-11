c131621c1d03416201f0dcc2ca40165bbb176c88
Eduard Ursu <eduardursu78@gmail.com>
2026-03-11 09:46:45 +0100

    v0

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,6 +3,7 @@ export { default as Badge } from "./Badge.astro";
 export { default as BadgeGroup } from "./BadgeGroup.astro";
 export { default as Button } from "./Button.astro";
 export { default as Card } from "./Card.astro";
+export { default as Dock } from "./Dock.astro";
 export { default as Drawer } from "./Drawer.astro";
 export { default as Filter } from "./Filter.astro";
 export { default as Icon } from "./Icon.astro";
